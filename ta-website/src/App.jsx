bd11c75c5b7dcdc75a76efa5a20049287988ea30
amankhan2005 <amankhan099k@gmail.com>
2026-06-13 03:00:05 +0530

    Fix DeleteAccount import

--- ta-website/src/App.jsx
+++ ta-website/src/App.jsx
@@ -15,8 +15,7 @@ import DownloadApp   from './pages/DownloadApp';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsPage     from './pages/TermsPage';
 import CookiePolicy  from './pages/CookiePolicy';
-import DeleteAccount from './pages/Deleteccount';
-
+import DeleteAccount from './pages/DeleteAccount';
 function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
